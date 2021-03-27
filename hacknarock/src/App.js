69ffc2f816df5c33a029f022e7470a1cd20784c3
PercentEquals <bartosz.nti@gmail.com>
2021-03-27 13:12:51 +0100

    Added map component with depedencies

--- hacknarock/src/App.js
+++ hacknarock/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import "./App.css";
 import { Button } from '@material-ui/core';
+import Map from './components/Map'
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 }
 
 export default App;
