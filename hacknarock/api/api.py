a4592037630105304d2577539cd75b952e2881cf
Rewzu <56927768+Rewzu@users.noreply.github.com>
2021-03-27 21:10:54 +0100

    Updated .gitignore

--- hacknarock/api/api.py
+++ hacknarock/api/api.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, make_response
 from database import db, Room, User, RoomUser, Message
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
