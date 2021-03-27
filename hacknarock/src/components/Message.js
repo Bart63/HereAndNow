3bf8762a2d53a150de7b1e86aa8ef611a6d6df1d
PercentEquals <bartosz.nti@gmail.com>
2021-03-27 14:57:45 +0100

    Added message component

---
+++ hacknarock/src/components/Message.js
@@ -0,0 +1,31 @@
+import { Paper } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		margin: 10
+	},
+	msg: {
+		flexGrow: 1,
+		padding: 10,
+		color: 'white'
+	}
+}));
+
+const Message = () => {
+	const classes = useStyles();
+
+	return (
+
+		<div className={classes.root}>
+			Anonymous:
+			<Paper className={classes.msg} elevation={1} style={{backgroundColor: '#757ce8'}}>
+
+				Hello!
+
+			</Paper>
+		</div>
+	)
+}
+
+export default Message
