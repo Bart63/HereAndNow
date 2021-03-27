b27fbc89e94fb7269db2a4624777643cf7575cbc
Bart63 <bartek.durys@gmail.com>
2021-03-27 19:53:35 +0100

    Script py to populate db

--- hacknarock/api/populate.py
+++ hacknarock/api/populate.py
@@ -5,6 +5,12 @@ import datetime
 if __name__=="__main__":
     with app.app_context():
         db.create_all()
+
+        meta = db.metadata
+        for table in reversed(meta.sorted_tables):
+            print(f"Clear table {table}")
+            db.session.execute(table.delete())
+        db.session.commit()
         
         ############## Room ##############
         rooms = []
@@ -48,7 +54,21 @@ if __name__=="__main__":
         messages.append(Message(author_id=1, room_id=1, data="Hello world", 
         creationDate=datetime.datetime(2021, 3, 27, 16, 23, 12, 123)))
         messages.append(Message(author_id=3, room_id=1, data="Hello hello", 
-        creationDate=datetime.datetime(2021, 3, 27, 16, 23, 12, 123)))
+        creationDate=datetime.datetime(2021, 3, 27, 16, 23, 13, 223)))
+        messages.append(Message(author_id=1, room_id=1, data="Noice!", 
+        creationDate=datetime.datetime(2021, 3, 27, 16, 23, 14, 123)))
+
+        messages.append(Message(author_id=2, room_id=2, data="I have an error", 
+        creationDate=datetime.datetime(2021, 3, 27, 16, 25, 14, 123)))
+        messages.append(Message(author_id=4, room_id=2, data="Lol, everything works", 
+        creationDate=datetime.datetime(2021, 3, 27, 16, 25, 22, 233)))
+        messages.append(Message(author_id=2, room_id=2, data="Not in my code :(", 
+        creationDate=datetime.datetime(2021, 3, 27, 16, 25, 32, 143)))
+
+        messages.append(Message(author_id=5, room_id=2, data="Is anybody here?", 
+        creationDate=datetime.datetime(2021, 3, 27, 16, 26, 12, 123)))
+        messages.append(Message(author_id=5, room_id=2, data="...", 
+        creationDate=datetime.datetime(2021, 3, 27, 16, 28, 23, 143)))
         
         for m in messages:
             db.session.add(m)
