f6e5a57510ec6d2ec6a27c80dcc9c3b573fed2ca
230045 Zagozda Konrad <230045@edu.p.lodz.pl>
2021-03-27 19:27:38 +0100

    ..

--- hacknarock/src/components/RoomsList.js
+++ hacknarock/src/components/RoomsList.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class RoomsList extends Component {
   render() {
-    return <div class="rooms-list"></div>;
+    return (
+      <div class="rooms-list">
+        <div></div>
+      </div>
+    );
   }
 }
