f3b2ed02d174c145d221d85b0978caa66bebb066
Bart63 <bartek.durys@gmail.com>
2021-03-27 12:57:54 +0100

    DB test

--- hacknarock/api/api.py
+++ hacknarock/api/api.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
 
 @app.route("/")
 def hello():
